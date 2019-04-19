9a71cc3681b5e26ee80b19e7fa242c9f5016ba78
Anjali Mac <anjali_mac@Anjalis-MacBook-Pro.local>
2019-04-19 14:02:08 -0400

    updated unique chars

--- src/main/java/com/crack/coding/ch1/Q1UniqueChars.java
+++ src/main/java/com/crack/coding/ch1/Q1UniqueChars.java
@@ -8,10 +8,14 @@ import java.util.Set;
 public class Q1UniqueChars {
 	
 	public static void main(String[] args) {
-		checkUniqueChars();
+		//checkUniqueChars();
+		
+		boolean res = updatedMethodForUniqueCheck(input);
+		
+		System.out.println("The string '"+input+"' has all unique chars: "+res);
 	}
 	
-	private static String input = "ejhjhfdff";
+	private static String input = "abcdzxnhkl";
 	private static Set unqSet = new HashSet();
 	
 	public static void checkUniqueChars() {
@@ -30,17 +34,35 @@ public class Q1UniqueChars {
 				//break;
 				
 			//}
-				
-			
+						
 			if(charSet[ch]) {
 				System.out.println("charset bool: "+ch);
 				break;
 			}
 			charSet[ch] = true;
 			
+		}		
+		
+	}
+	
+	public static boolean updatedMethodForUniqueCheck(String input) {
+		
+		if(input.length()>128) {
+			return false;
 		}
 		
+		boolean[] charSet = new boolean[128];
+		for(int i=0;i<input.length();i++) {
+			int val = input.charAt(i);
+			
+			if(charSet[val]) {
+				return false;
+			}
+			
+			charSet[val] = true;
+		}
 		
+		return true;
 	}
 
 }
