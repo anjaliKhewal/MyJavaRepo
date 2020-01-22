847194d36b4314446f8c473a7c8c10e5d217e2b6
Anjali Kumar <anjali.kumar@nielsen.com>
2020-01-21 19:08:05 -0500

    day1 test

---
+++ src/main/java/com/leetcode/Ques13_Roman_Integer.java
@@ -0,0 +1,32 @@
+package com.leetcode;
+
+public class Ques13_Roman_Integer {
+	
+	public static void main(String[] args) {
+		
+		romanToInt("XV");
+		
+	}
+	
+	private int I = 1;
+	private int V = 5;
+	private int X = 10;
+	private int L = 50;
+	private int C = 100;
+	private int D = 500;
+	private int M = 1000;
+	
+	 public static int romanToInt(String s) {
+		 
+        char[] charArr = s.toCharArray();
+        
+        for(char ch: charArr) {
+        	
+        	
+        }
+	 return 0;
+	 }
+	 
+	 
+
+}
